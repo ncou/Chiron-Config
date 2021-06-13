9d989f95838cc4cfdb2a1bdf72092f2dd647cfbc
ncou <nicolas_courade@yahoo.fr>
2021-06-13 17:24:04 +0200

    update

--- src/InjectableConfigInterface.php
+++ src/InjectableConfigInterface.php
@@ -5,6 +5,9 @@ declare(strict_types=1);
 namespace Chiron\Config;
 
 // TODO : éventuellement séparer le code et les interfaces en deux parties, une pour la partie "Modification" avec les méthodes reset/set/addConfig, et une partie "Injection" avec la méthode getConfigSectionName()
+
+// TODO : il faudrait que cette interface étende da l'interface : ConfigInterface !!! car on a besoin d'utiliser la méthode get() par exemple !!!!
+
 interface InjectableConfigInterface
 {
     public function getConfigSectionName(): string;
